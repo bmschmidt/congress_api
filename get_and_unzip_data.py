4caa113801724a02351126f1ee80d28e4501f564
Matt Nicklay <matthew.nicklay@gmail.com>
2013-11-27 15:47:11 -0600

    Update get_and_unzip_data.py

--- get_and_unzip_data.py
+++ get_and_unzip_data.py
@@ -1,23 +1,22 @@
-from multiprocessing import Pool,cpu_count
-from os.path import abspath,dirname
-from subprocess import call
-from urllib import urlretrieve
+import multiprocessing
+import os
+import subprocess
+import urllib
 
-basedir = abspath(dirname(__file__))
-baseurl = 'http://unitedstates.sunlightfoundation.com/congress/data'
+basedir = os.path.abspath(os.path.dirname(__file__))
+baseURL = 'http://unitedstates.sunlightfoundation.com/congress/data'
 
 
-def getData(idx):
-    thisurl = '%s/%s.zip' % (baseurl, idx)
-    thiszip = '%s/%s/%s.zip' % (basedir, idx, idx)
-    thisdir = '%s/%s' % (basedir, idx)
-    call(['mkdir', thisdir])
-    urlretrieve(thisurl, thiszip)
-    call(['unzip', thiszip, '-d', thisdir])
-    call(['rm', thiszip])
-    print 'FINISHED: %s' % thiszip.split('/')[-1]
+def getCongressData(congress_num):
+    zip_path = '%s/%s/%s.zip' % (basedir, congress_num, congress_num)
+    outdir = '%s/%s' % (basedir, congress_num)
+    subprocess.call(['mkdir', outdir])
+    urllib.urlretrieve('%s/%s.zip' % (baseURL, congress_num), zip_path)
+    subprocess.call(['unzip', '-qq', zip_path, '-d', outdir])
+    subprocess.call(['rm', zip_path])
+    print 'DONE: %s' % zip_path.split('/')[-1]
 
 if __name__ == '__main__':
-    pool = Pool(processes=cpu_count())
-    pool.map(getData, range(93, 114))
-    
+    pool = multiprocessing.Pool()
+    pool.map(getCongressData, range(93, 114))
+    pool.close()
